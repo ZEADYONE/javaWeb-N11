143bbf483e0c0acbff601cdfe2a4d66996c0b438
Tung <TungNVT.B23CN902@stu.ptit.edu.vn>
2025-10-09 17:09:29 +0700

    add domain

--- src/main/java/com/n11/sportshop/domain/User.java
+++ src/main/java/com/n11/sportshop/domain/User.java
@@ -1,11 +1,75 @@
 package com.n11.sportshop.domain;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="Users")
 public class User {
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private int user_id;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+    
+    private String username;
+    private String password;
     private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    public int getUser_id() {
+        return user_id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+
 }
