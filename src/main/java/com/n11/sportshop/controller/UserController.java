bcba87e7ee31cbb332a22434d5e7cb3a005c6518
NTG259 <truonggiang29122005@gmail.com>
2025-10-09 21:41:02 +0700

    create User

--- src/main/java/com/n11/sportshop/controller/UserController.java
+++ src/main/java/com/n11/sportshop/controller/UserController.java
@@ -3,11 +3,21 @@ package com.n11.sportshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.n11.sportshop.domain.User;
+import com.n11.sportshop.service.UserService;
 
 @Controller
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/admin/user")
     public String getUserPage() {
         return "admin/user/show";
@@ -18,4 +28,11 @@ public class UserController {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
+
+    @PostMapping("/admin/user/create")
+    public String postCreateUser(@ModelAttribute("newUser") User user) {
+        this.userService.saveUser(user);
+        return "redirect:/admin/user";
+    }
+
 }
--- src/main/java/com/n11/sportshop/service/UserService.java
+++ src/main/java/com/n11/sportshop/service/UserService.java
@@ -2,7 +2,19 @@ package com.n11.sportshop.service;
 
 import org.springframework.stereotype.Service;
 
+import com.n11.sportshop.domain.User;
+import com.n11.sportshop.repository.UserRepository;
+
 @Service
 public class UserService {
-    
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public void saveUser (User user) {
+        this.userRepository.save(user);
+    }
 }
