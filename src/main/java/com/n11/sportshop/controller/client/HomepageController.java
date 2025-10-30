16df0f19185b756b0410dec7425555aad7704e3a
daubong240705-art <daubong240705@gmail.com>
2025-10-30 20:58:32 +0700

    up

--- src/main/java/com/n11/sportshop/controller/client/HomepageController.java
+++ src/main/java/com/n11/sportshop/controller/client/HomepageController.java
@@ -69,9 +69,7 @@ public class HomepageController {
 
     @GetMapping("/access-deny")
     public String getDenyPage(Model model) {
-        return "client/auth/deny";
+        return "redirect:https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1";
     }
-
-    
 
 }
