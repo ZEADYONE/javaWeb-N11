8224a44e1219cd532fd4a3365f816f0579aa0f96
Tung <TungNVT.B23CN902@stu.ptit.edu.vn>
2025-11-04 22:58:03 +0700

    Update

--- src/main/java/com/n11/sportshop/controller/admin/UserController.java
+++ src/main/java/com/n11/sportshop/controller/admin/UserController.java
@@ -102,19 +102,19 @@ public class UserController {
     }
 
     // Delete-User
-    @GetMapping("/delete/{id}")
-    public String getDeleteUserPage(Model model, @PathVariable int id) {
-        User user = this.userService.getUserByID(id);
-        model.addAttribute("user", user);
-        return "admin/user/delete";
-    }
+    // @GetMapping("/delete/{id}")
+    // public String getDeleteUserPage(Model model, @PathVariable int id) {
+    //     User user = this.userService.getUserByID(id);
+    //     model.addAttribute("user", user);
+    //     return "admin/user/delete";
+    // }
 
-    @PostMapping("/delete")
-    public String postMethodName(Model model, @ModelAttribute("user") User user) {
-        // Chỉ xóa ảnh nếu KHÔNG phải ảnh mặc định
-        this.userService.deleteUser(user);
-        return "redirect:/admin/user";
-    }
+    // @PostMapping("/delete")
+    // public String postMethodName(Model model, @ModelAttribute("user") User user) {
+    //     // Chỉ xóa ảnh nếu KHÔNG phải ảnh mặc định
+    //     this.userService.deleteUser(user);
+    //     return "redirect:/admin/user";
+    // }
 
     // Detail-User
     @GetMapping("/detail/{id}")
@@ -151,14 +151,10 @@ public class UserController {
         return "redirect:/admin/user";
 
     }
-    @GetMapping
-    public String getUserList(Model model) {
-        model.addAttribute("users", userService.getActiveUsers());
-        return "admin/user/show";
-    }
+    
 
-    @PostMapping("/delete")
-    public String deleteUser(@RequestParam("id") Integer id) {
+    @PostMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Integer id) {
         userService.softDeleteUser(id);
         return "redirect:/admin/user";
     }
