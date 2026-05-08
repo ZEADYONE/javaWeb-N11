204e26778846b15925a29e8341852b4ce465630d
Cuong <tricuongptdtntsd@gmail.com>
2026-05-08 07:27:24 +0700

    dd

--- src/main/java/com/n11/sportshop/service/CategoryService.java
+++ src/main/java/com/n11/sportshop/service/CategoryService.java
@@ -38,11 +38,11 @@ public class CategoryService {
         this.categoryRepository.save(category);
     }
 
-    public void updateCategory(Integer id, Category Category) {
+    public void updateCategory(Integer id, Category updatedCategory) {
         Category category = categoryRepository.findById(id).orElse(null);
         if (category != null) {
-            category.setName(category.getName());
-            category.setCode(category.getCode());
+            category.setName(updatedCategory.getName());
+            category.setCode(updatedCategory.getCode());
             categoryRepository.save(category);
         }
     }
